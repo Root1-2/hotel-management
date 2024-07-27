1cc19a022da01f3a495f3ad009c1ee8aabcecd3a
John Doe <abdullahalmasrur8@gmail.com>
2024-07-28 01:10:50 +0600

    using props in styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -19,10 +14,13 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hotel Management</H1>
+        <Heading as="h1">Hotel Management</Heading>
+
+        <Heading as="h2">Check In & Out</Heading>
         <Button onClick={() => alert("Check In")}>Check In</Button>
         <Button onClick={() => alert("Check Out")}>Check Out</Button>
 
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of Guests" />
       </StyledApp>
     </>
