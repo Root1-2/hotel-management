fc1917d79f52bf6cd6fe505b466ed9a834257886
John Doe <abdullahalmasrur8@gmail.com>
2024-07-30 22:41:30 +0600

    Login Form + functionalities

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
+import { useLogin } from "./useLogin";
+
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, isPending } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!email || !password) return;
+
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -16,6 +26,7 @@ function LoginForm() {
         <Input
           type="email"
           id="email"
+          disabled={isPending}
           // This makes this form better for password managers
           autoComplete="username"
           value={email}
@@ -29,10 +40,13 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isPending}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isPending}>
+          {!isPending ? "Log In" : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -21,8 +21,9 @@ const Heading = styled.h1`
   ${(props) =>
     props.as === "h3" &&
     css`
-      font-size: 2rem;
+      font-size: 2.5;
       font-weight: 500;
+      text-align: center;
     `}
   line-height: 1.4
 `;
